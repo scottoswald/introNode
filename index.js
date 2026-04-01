bc229771c52a4ba4b09fe6a79dd3845e51deb717
Scott Oswald <scottoswald1@gmail.com>
2026-04-01 17:53:43 +0100

    add imports

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 #!/usr/bin/env node
 
+import whatever, {count} from './utils.js';
+
 console.log('hello world');
 
 const note = process.argv[2];
@@ -7,6 +9,8 @@ const note = process.argv[2];
 const newNote = {
   id: Date.now(),
   note,
+  whatever: whatever.name,
+  count: count(5)
 };
 
 console.log('your new note', newNote)
