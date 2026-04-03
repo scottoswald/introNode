dd008dca5ce5e702df8720f44e921909085cc6c0
Scott Oswald <scottoswald1@gmail.com>
2026-04-03 01:23:34 +0100

    delete old code, now it;s just importing files and is entry point to app

--- index.js
+++ index.js
@@ -1,21 +1,3 @@
 #!/usr/bin/env node
 
-import whatever, {count} from './utils.js';
-import * as modules from './modules/index.js';
-
-console.log('hello world');
-
-const note = process.argv[2];
-
-const newNote = {
-  id: Date.now(),
-  note,
-  whatever: whatever.name,
-  count: count(5),
-  age: modules.age(30),
-  firstName: modules.firstName('Phil'),
-  secondName: modules.secondName('O\'Brian')
-};
-
-console.log('your new note', newNote)
-
+import './src/command.js';
